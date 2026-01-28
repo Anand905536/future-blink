2d9dc78ea550c3ce0974e09faddf0ef90e323815
anand <aak905536@gmail.com>
2026-01-28 10:52:15 +0530

    client initiated

---
+++ client/ai_flow/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>welcome to the project</div>
+       
+    </>
+  )
+}
+
+export default App
