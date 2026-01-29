d4becf6be1b2015e47991d030dd811eb7c034f03
anand <aak905536@gmail.com>
2026-01-29 15:35:57 +0530

    Updated API URL in services.js

--- client/ai_flow/src/services/services.js
+++ client/ai_flow/src/services/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api"
+    baseURL: "https://future-blink-5zld.onrender.com/api"
 })
 
 export const askAI = (prompt) => {
